7c0a53f180e98f33394d95609d056ecc85241429
Mateo <20204327@aloe.ulima.edu.pe>
2023-09-06 19:36:36 -0500

    20204327

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function sumar() {
+    let v1 = document.getElementById("op1").value
+    let v2 = document.getElementById("op2").value
+    let resultado = parseInt(v1) + parseInt(v2)
+    document.getElementById("rpta").innerHTML = resultado
+
+    
+}
